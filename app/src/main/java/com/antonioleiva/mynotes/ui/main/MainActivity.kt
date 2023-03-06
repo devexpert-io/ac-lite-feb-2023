eebdf770f4561f35a83bcb0cd8797ee5483ba9ce
antoniolg <antonio@devexperto.com>
2023-03-06 17:23:35 +0100

    Architect Coders Lite: Día 4 - Testing

--- app/src/main/java/com/antonioleiva/mynotes/ui/main/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mynotes/ui/main/MainActivity.kt
@@ -6,12 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.antonioleiva.mynotes.NotesApplication
-import com.antonioleiva.mynotes.data.NotesRepository
-import com.antonioleiva.mynotes.data.NotesRoomDataSource
 import com.antonioleiva.mynotes.databinding.ActivityMainBinding
-import com.antonioleiva.mynotes.domain.DeleteNoteUseCase
-import com.antonioleiva.mynotes.domain.GetCurrentNotesUseCase
 import com.antonioleiva.mynotes.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
--- app/src/test/java/com/antonioleiva/mynotes/ExampleUnitTest.kt
+++ app/src/test/java/com/antonioleiva/mynotes/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.antonioleiva.mynotes
 
+import com.antonioleiva.mynotes.data.NotesLocalDataSource
+import com.antonioleiva.mynotes.data.NotesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +16,48 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `When note is saved, the note is returned in the flow`() = runBlocking {
+        val ds = FakeLocalDataSource()
+        val repository = NotesRepository(ds)
+
+        repository.save(Note(1, "Title 1", "Description 1"))
+
+        val result = repository.currentNotes.first()
+        assertEquals(1, result[0].id)
     }
+
+    @Test
+    fun `When a note is deleted, the repository returns the list without the note`() = runBlocking {
+        val note = Note(1, "Title 1", "Description 1")
+        val ds = FakeLocalDataSource(listOf(note))
+        val repository = NotesRepository(ds)
+
+        repository.delete(note)
+
+        val result = repository.currentNotes.first()
+        assertEquals(emptyList<Note>(), result)
+    }
+}
+
+class FakeLocalDataSource(initialNotes: List<Note> = emptyList()) : NotesLocalDataSource {
+    private val mutableNotes = MutableStateFlow(initialNotes)
+    override val currentNotes: Flow<List<Note>> = mutableNotes
+
+    override suspend fun delete(note: Note) {
+        mutableNotes.value = mutableNotes.value - note
+    }
+
+    override suspend fun getById(noteId: Int): Note? = mutableNotes.value.find { it.id == noteId }
+
+    override suspend fun save(note: Note) {
+        val list = mutableNotes.value.toMutableList()
+        val index = list.indexOfFirst { it.id == note.id }
+        if (index >= 0) {
+            list[index] = note
+        } else {
+            list.add(note)
+        }
+        mutableNotes.value = list
+    }
+
 }
